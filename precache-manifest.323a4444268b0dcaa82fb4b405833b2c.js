5288d6429836edb4854f23292a57b8709f099a92
Abdu Assabri <abdusabri@outlook.com>
2019-01-16 22:53:44 +0100

    Updates

---
+++ precache-manifest.323a4444268b0dcaa82fb4b405833b2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2b75e70e11da2515d13",
+    "url": "/fcc-pomodoro-fix/static/css/main.c5757bb7.chunk.css"
+  },
+  {
+    "revision": "e2b75e70e11da2515d13",
+    "url": "/fcc-pomodoro-fix/static/js/main.e2b75e70.chunk.js"
+  },
+  {
+    "revision": "0a103af7bfbcd98565f6",
+    "url": "/fcc-pomodoro-fix/static/js/1.0a103af7.chunk.js"
+  },
+  {
+    "revision": "ed080dc9245e172fdc70",
+    "url": "/fcc-pomodoro-fix/static/js/runtime~main.ed080dc9.js"
+  },
+  {
+    "revision": "a34b5e078175240be45daa4956ce5c06",
+    "url": "/fcc-pomodoro-fix/index.html"
+  }
+];
